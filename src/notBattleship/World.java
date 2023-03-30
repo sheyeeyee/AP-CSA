829ad7113c355dc5c6c7d6a861e51b5d9c0d7257
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-03-30 12:45:36 -0700

    wakdfjakwlejlkfjwekjflkdsf

--- src/notBattleship/World.java
+++ src/notBattleship/World.java
@@ -1,10 +1,23 @@
 package notBattleship;
 
 public class World {
+	private String[][] map;
+	
+	public static int NORTH = 0;
+	public static  int NORHTEAST = 1;
+	public static  int EAST = 2;
+	public static  int SOUTHEAST = 3;
+	public static  int SOUTH = 4;
+	public static  int SOUTHWEST = 5;
+	public static  int WEST = 6;
+	public static  int NORTHWEST = 7;
+	
+	public World() {
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
